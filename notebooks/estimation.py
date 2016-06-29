f77655c836fe5264a22c9664849243592ff58299
amaurydurand <durand_amaury@hotmail.fr>
2016-06-29 18:49:14 +0200

    debug + TVAR audio fonctionne pas

--- notebooks/estimation.py
+++ notebooks/estimation.py
@@ -27,39 +27,43 @@ def generation_est(X, d, T, mu):
     return X_est, theta_est
 
 def generation_est2(X, d, T, mu):
+    theta_est = np.zeros((d,T),dtype='double')
+    X_pred = np.zeros(T,dtype='double')
+    XX = X[0:d][::-1]
+    for t in np.arange(d,T):
+        XX = X[t-d:t][::-1]
+        X_pred[t] = np.dot(theta_est[:,t-1].T,XX)
 
-    theta_est = np.zeros((d,T),dtype='complex')
-    X_est = np.zeros(T,dtype='complex')
+        err = X[t]-X_pred[t]
         
-    for k in (np.arange(d,T)):
-        XX = X[k-d:k][::-1]
-        
-        theta_est[:,k] = np.matrix(theta_est[:,k-1] + mu * (X[k] - np.dot(XX, theta_est[:,k-1])) * XX / (1 + mu * norm(XX) ** 2)) 
-
-        X_est[k] = np.dot(XX, theta_est[:,k])
-        
-    return X_est, theta_est
-
+        theta_est[:,t] = theta_est[:,t-1] + mu*XX/(1+mu*np.linalg.norm(XX)**2)*err
+    return X_pred, theta_est
 
 def aggregation(X, predictions, estimations, d, T, eta, strategy = 1):    
-    """Retourne un predicteur par aggregation"""   
-    N = len(predictions[:,0])
-    pred = np.zeros(T,dtype='complex')
-    estim = np.zeros((d,T),dtype='complex')
-    alpha = 1./N * np.ones(N)   
+    # predictions : N*T
+    # estimations : N*d*T
+    N = predictions.shape[0]
+    pred = np.zeros(T,dtype='double')
+    estim = np.zeros((d,T), dtype='double')
+    # Initialisation :
+    alpha = 1./N*np.ones(N,dtype='double')
+    pred[0] = np.dot(predictions[:,0].T,alpha)
+    estim[:,0] = np.dot(estimations[:,:,0].T,alpha)
     
-    for t in np.arange(T): 
-        if t > 0:
-            if strategy == 1:
-                v = alpha * np.exp(-2*eta*(pred[t-1]-X[t-1])*predictions[:,t-1])
+    # Mise a jour :
+    for t in np.arange(1,T):
+        v = np.zeros(N,dtype='double')
+        for i in np.arange(N):
+            if strategy==1:
+                v[i]=alpha[i]*np.exp(-2*eta*(pred[t-1]-X[t-1])*predictions[i,t-1])
             else:
-                v = alpha * np.exp(-eta*(predictions[:,t-1]-X[t-1])**2)
+                v[i]=alpha[i]*np.exp(-eta*(predictions[i,t-1]-X[t-1])**2)
+        alpha= v / np.sum(v)
         
-            alpha = v / np.sum(v)           
-        pred[t] = np.dot(alpha, predictions[:,t])  
-        estim[:,t] = np.dot(estimations[:,:,t].T,alpha) 
+        pred[t] = np.dot(predictions[:,t].T,alpha)
+        estim[:,t] = np.dot(estimations[:,:,t].T,alpha)
         
-    return pred, estim, alpha
+    return pred,estim
 
 
 def dsp(theta, T, N = 512):
@@ -71,16 +75,15 @@ def dsp(theta, T, N = 512):
 
 def do_aggregation(X,d,T,eta,mu,strategy=1):
     N = len(mu)
-    X_pred = np.zeros((N,T),dtype='complex')
-    theta_estim = np.zeros((N,d,T),dtype='complex')
+    X_pred = np.zeros((N,T),dtype='double')
+    theta_estim = np.zeros((N,d,T),dtype='double')
 
 
     for k in np.arange(N):
         X_pred[k,:],theta_estim[k,:,:] = generation_est2(X, d, T, mu[k])
 
-    pred_agr, estim_agr, alpha = aggregation(X, X_pred, theta_estim, d,T, 0.1, 1)
-        
-                  
-    return pred_agr, estim_agr, alpha
+    pred_agr, estim_agr = aggregation(X, X_pred, theta_estim, d,T, 0.1, strategy)
+                     
+    return pred_agr, estim_agr
                            
     
